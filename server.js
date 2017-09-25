58298945a4e50b76a346a37143ce8e4c1c6bcbbd
Axel <axelfuh@gmail.com>
2017-09-25 11:27:30 +0200

    Set up MongoDB

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
-const express = require('express')
+const mongoose = require('mongoose');
+require('dotenv').config({ path: 'variables.env' });
+
+mongoose.connect(process.env.DATABASE);
+mongoose.Promise = global.Promise;
+mongoose.connection.on('error', (err) => {
+  console.error(`${err.message}`);
+});
+
+
 const app = require('./app');
 
 const PORT = process.env.PORT || 8080;
