f269902b2f3bd43741e87b3be10662d60045cd6c
Oliver Weber <oliver@mondough.io>
2017-09-25 17:11:03 +0200

    Revert "Merge branch 'develop' into ayham"
    This reverts commit a7fd1f9cc5f66d354cdc97f58812d0fe2e26f78a, reversing
    changes made to 475c91e8f0e5a189d641a7ad0ded072787cec3c7.

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
-var express    	    = require('express');
-var app        	    = express();
-var bodyParser 	    = require('body-parser');
-var mongoose        = require('mongoose');
-var Post 		    = require("./models/Post.js");
-var postRoutes      = require('./routes/postRouters');
-var commentRoutes   = require('./routes/commentRoutes');
-var pictureRoutes   = require('./routes/pictureRoutes');
+var express    	= require('express');        
+var app        	= express();               
+var bodyParser 	= require('body-parser');
+var db 			= require("./config/mongoose/database.js");
+var Post 		= require("./models/Post.js");
+var postRoutes = require('./routes/postRouters');
+var commentRoutes = require('./routes/commentRoutes');
+var pictureRoutes = require('./routes/pictureRoutes');
 
-require('dotenv').config({ path: 'variables.env' });
+var port = process.env.PORT || 8080;
 
-// Database Section
-mongoose.connect(process.env.DATABASE, {
-    useMongoClient: true,
-    promiseLibary: global.Promise
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+  
+
+app.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
 });
 
-mongoose.connection.on('error', (err) => {
-  console.error(`${err.message}`);
+postRoutes(app);
+commentRoutes(app);
+pictureRoutes(app);
+
+console.log('Magic happens on port ' + port);
+app.use(function(req, res) {
+  res.status(404).send({url: req.originalUrl + ' not found'})
 });
+
 
 db.on('error', console.error.bind(console, 'conection error:'));
 db.once('open', function(){
@@ -25,23 +33,6 @@ db.once('open', function(){
 });
 
 
-// pre Middleware Section
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.listen(port);
+module.exports = app;
 
-// Routes
-app.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
-});
-
-postRoutes(app);
-commentRoutes(app);
-pictureRoutes(app);
-
-// 404
-app.use(function(req, res) {
-  res.status(404).send({url: req.originalUrl + ' not found'})
-});
-
-// App listens
-app.listen(process.env.PORT, () => console.log('\x1b[33m%s\x1b[0m', `Express running → PORT ${process.env.PORT}`))
