888b44675055b6534a4e242365030cea67248086
paloma <palomita.c1@gmail.com>
2017-09-25 15:16:12 +0200

    add post, update, delete and read function in controllers

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -6,17 +6,51 @@ var mongoose = require("mongoose"),
 
 exports.list_all_posts = function(req, res) {
 	Post.find({}, function(err, result) {
-		if (err) res.send(err);
+		if (err)
+		 res.send(err);
 		res.json(result);
 
 		console.log(result);
 	});
 };
 
-/*exports.create_a_posts = function(req, res) {
+exports.create_a_post = function(req, res) {
 	var new_post = new Post(req.body);
-	new_post.save(function(error, result) {
-		if (err) res.send(err);
+	new_post.save(function(err, result) {
+		if (err)
+		 res.send(err);
 		res.json(result);
 	});
-};*/
+};
+
+exports.update_a_post = function(req, res){
+	Post.findOneAndUpdate({
+		_id: req.params.postId
+	},
+	 req.body, {new:true}, function(err, result){
+		if(err)
+			res.send(err);
+		res.json(result);
+	});
+};
+
+exports.delete_a_post = function(req, res){
+	Post.remove({
+		_id:req.params.postId
+	}, function(err, result){
+		if (err)
+			res.send(err);
+		res.json({message:'Post successfully delete'});
+
+	});
+};
+
+exports.read_a_post = function(req, res){
+	Post.findById(req.params.postId, function(err, result){
+		if(err)
+			res.send(err);
+		res.json(result);
+	
+	});
+};
+
