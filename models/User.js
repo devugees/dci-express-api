b8ec56c3059fea5d260e42f47da3ebda8b5522b9
Axel <axelfuh@gmail.com>
2017-09-28 19:34:25 +0200

    Rework User model to match the Github info

--- models/User.js
+++ models/User.js
@@ -1,11 +1,26 @@
-const mongoose     = require('mongoose');
+const mongoose = require('mongoose');
+const findOrCreate = require('mongoose-findorcreate')
 
 
-const userSchema   = new mongoose.Schema({
-    first_name: String,
-    last_name:  String,
-    user_email: String,
-    age:{type:Number,min:18,max:60}
+// const userSchema   = new mongoose.Schema({
+//     first_name: String,
+//     last_name:  String,
+//     user_email: String,
+//     age:{type:Number,min:18,max:60}
+// });
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  username: String, // <- this is the url
+  avatar_url: String,
+  github_url: String,
+  website: String,
+  created: {
+    type: Date,
+    default: Date.now
+  }
 });
+
+userSchema.plugin(findOrCreate);
 
 module.exports = mongoose.model('User', userSchema);
