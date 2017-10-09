cd8743ed996eb41f58613b97afa590a8297b069f
ns4000 <ns40002@gmail.com>
2017-10-09 10:54:06 +0200

    removed adding/deleteing user cuz we dont need them and fixed save comnt

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -5,35 +5,29 @@ exports.showUsers = async ( req , res ) => {
   res.json(users);
 }
 
-exports.addUser = async (req, res) => {
-  const user = await new User({first_name:req.body.first_name,last_name:req.body.last_name,user_email:req.body.user_email,age:req.body.age}).save();
-  res.json({user});
-}
+// exports.addUser = async (req, res) => {
+//   const user = await new User({first_name:req.body.first_name,last_name:req.body.last_name,user_email:req.body.user_email,age:req.body.age}).save();
+//   res.json({user});
+// }
 
 exports.getUser = async(req, res) =>{
 const user = await User.findOne({_id: req.params.user_id});
-
 const message = {error : "user not found"};
-
-res.json(!user ? message : user)
+  res.json(!user ? message : user)
 }
 
 exports.removeUser = async (req,res)=> {
 const user = await User.findOne({_id:req.params.user_id})
-user.remove();
-
-res.json({message:'user deleted'});
-
+  user.remove();
+  res.json({message:'user deleted'});
 }
 
-exports.updateUser = async (req, res) =>{
-  const user = await User.findOneAndUpdate(
-    {_id:req.params.user_id},
-    {$set:{first_name:req.body.first_name}},
-    {new:true}
-  );
-
-  const message = {error : "user not found"};
-
-  res.json(!user ? message : user);
-}
+// exports.updateUser = async (req, res) =>{
+//   const user = await User.findOneAndUpdate(
+//     {_id:req.params.user_id},
+//     {$set:{first_name:req.body.first_name}},
+//     {new:true}
+//   );
+//   const message = {error : "user not found"};
+//   res.json(!user ? message : user);
+// }
