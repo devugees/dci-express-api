24ab229a5558ba3f4c88ca2bcb205eaa0280873f
Unknown <palomita.c1@gmail.com>
2017-10-09 12:38:53 +0200

    update routes category

--- routes/categoryRouters.js
+++ routes/categoryRouters.js
@@ -3,11 +3,11 @@
 module.exports = function(app){
   var category= require('../controllers/categoryControllers');
 
-  app.route('/category')
+  app.route('/api/category')
     .get(category.list_all_Categorys)
    // .post(category.create_a_Category);
 
-  app.route('/category/:CategoryId')
+  app.route('/api/category/:CategoryId')
   //  .put(category.update_a_Category)
  //   .delete(category.delete_a_Category)
     .get(category.read_a_Category);
