8b995d985782bc8ff3a3b3b5a11c696375ed1138
ayhamk94 <ayham.kteash1995@gmail.com>
2017-10-11 11:15:21 +0200

    Update: README.me and change the Variables.env to .env

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,6 +2,10 @@ module.exports = (app) => {
   const auth = require('../controllers/authControllers')
 
   app.route('/login').get(auth.login)
-  app.route('/gh').get(auth.authenticate)
   app.route('/logout').get(auth.logout)
+
+  app.route('/gh').get(auth.authenticate, (req, res) => {
+    res.redirect(req.session.returnTo || '/');
+    delete req.session.returnTo
+  })
 };
