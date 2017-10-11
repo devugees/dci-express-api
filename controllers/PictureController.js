2f019acba06e822587cc5445847f73b002f0d473
Thomas Kuhnert <spiel.hoelle@live.de>
2017-10-11 15:04:27 +0200

    fix comment association

--- controllers/PictureController.js
+++ controllers/PictureController.js
@@ -1,43 +1,9 @@
-const Picture = require('../models/Picture');
-const fs = require('fs');
-const multer = require('multer');
-const crypto = require('crypto');
-const path = require('path');
-require('dotenv').config({path: '.env'});
+var Picture = require('../models/Picture');
+const Comment = require('../models/Comment');
+var fs = require('fs');
 
-// multer config for renaming files
-const storage = multer.diskStorage({
-  destination: process.env.UPLOADSFOLDER,
-  filename: function(req, file, cb) {
-    crypto.pseudoRandomBytes(16, function(err, raw) {
-      if (err)
-        return cb(err)
-
-      cb(null, Date.now() + raw.toString('hex') + path.extname(file.originalname))
-    })
-  }
-});
-// multer config for handling file extenions
-const allowedExtension = (req, file, cb) => {
-  if (!file.mimetype.startsWith('image/')) {
-    return cb(new Error('Only image files are allowed!'), false);
-  }
-  cb(null, true);
-}
-// multer settings
-
-
-/*======= Upload Image Handler =========*/
-exports.uploadImage = multer({
-  storage: storage,
-  limits: {
-    fileSize: 10485760
-  },
-  fileFilter: allowedExtension
-}).single('image');
-/*======== Save Image Handler ==============*/
-exports.saveImage = async(req, res) => {
-  req.body.path = req.file.path;
+exports.uploadPicture = async(req, res) => {
+  req.body.path = req.file.path
   req.body.author = req.user._id;
 
   const picture = await (new Picture(req.body)).save();
@@ -45,7 +11,7 @@ exports.saveImage = async(req, res) => {
   res.redirect(`back`)
 }
 
-exports.handeUpdatedImage = async(req, res) => {
+exports.updatePicture = async(req, res) => {
   const picture = await Picture.findById(req.params.id);
   fs.unlink(picture.path);
   Picture.update(picture, {path: req.file.bath});
@@ -66,15 +32,13 @@ exports.listAll = async(req, res) => {
   res.json(pictures);
 
 }
-exports.findImageById = async(req, res) => {
+exports.findPictureById = async(req, res) => {
   const picture = await Picture.findById(req.params.id);
   res.json(picture)
 
 }
-exports.findImageByIdWithComments = (req, res) => {
-  Comment.find({
-    "pictureid": req.params.id
-  }, (err, comment) => {
+exports.findPictureByIdWithComments = (req, res) => {
+  Comment.find({ "pictureid": req.params.id }, (err, comment) => {
     if (err)
       res.json(err);
     res.json(comment);
